1f509d84b51e482956269da1fbe18c20fb8d3ac3
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-10-28 17:40:21 +0300

    Forms example. Output parammeters example.

---
+++ src/app/add-user/add-user.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { IUser } from '../user/user';
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent {
+
+  creationForm = this.fb.group({
+    name: [''],
+    email: [''],
+    birthdate: [new Date()],
+    isAdmin: [false]
+  });
+
+  @Output() 
+  createEvent = new EventEmitter<IUser>();
+
+  constructor(private fb: FormBuilder) { }
+  
+  onSubmit(): void {
+
+    if (!this.creationForm.valid) {
+      alert("Invalid data!");
+      return;
+    }
+
+    const user = this.creationForm.value as IUser;
+    console.log(user);
+
+    this.createEvent.emit(user);
+  }
+}
